b7b838405d19499e2acd77d31eee90a6d055786b
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2022-05-05 15:51:00 +0200

    [OTHER] rename apps in project with generic name

--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'swipper',
+  displayName: 'frontend',
   preset: '../../jest.preset.ts',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/swipper',
+  coverageDirectory: '../../coverage/apps/frontend',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
